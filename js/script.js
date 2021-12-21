281ba9b405ac7af9d2e85c8bcce43dcef39bd501
GiulioSpugnini <giuliospugnini@yahoo.com>
2021-12-21 15:11:58 +0100

    added steps of work

--- js/script.js
+++ js/script.js
@@ -1,1 +1,12 @@
 console.log('JS OK!');
+
+/*
+PASSAGGI:
+    1. Chiedere quanti km l'utente vyole percorrere
+    2. Chiedere l'eta del passegero
+    3. Calcolare il prezzo totale del viaggio
+    3a. Calcolare prezzo €/km
+    3b. Applicare sconto 20% per i minorenni
+    3c. Applicare sconto 40% per gli over 65
+    4. Ottenere output del prezzo finale con massimo due decimali
+*/
