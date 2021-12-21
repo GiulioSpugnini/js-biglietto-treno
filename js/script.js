1b64d33716a7798706f5a1289e146d541531724a
GiulioSpugnini <giuliospugnini@yahoo.com>
2021-12-21 15:21:08 +0100

    added 1st step

--- js/script.js
+++ js/script.js
@@ -2,11 +2,19 @@ console.log('JS OK!');
 
 /*
 PASSAGGI:
-    1. Chiedere quanti km l'utente vyole percorrere
+    1. Chiedere quanti km l'utente vuole percorrere
     2. Chiedere l'eta del passegero
     3. Calcolare il prezzo totale del viaggio
-    3a. Calcolare prezzo €/km
-    3b. Applicare sconto 20% per i minorenni
-    3c. Applicare sconto 40% per gli over 65
+        3a. Calcolare prezzo €/km
+        3b. Applicare sconto 20% per i minorenni
+        3c. Applicare sconto 40% per gli over 65
     4. Ottenere output del prezzo finale con massimo due decimali
 */
+
+// 1. Chiedere quanti km l'utente vuole percorrere
+const userJourney = parseInt(prompt('Quanti km vuoi percorrere? '));
+if (isNaN(userJourney)) {
+    alert("Hai inserito l'input sbagliato, ti prego di riprovare");
+    window.location.reload();
+}
+console.log(userJourney);
